00cc846c6382974fcd0fb3554193a2eb9f491b58
tganyan <tganyan@gmail.com>
2018-11-13 12:40:46 -0800

    updated logic for diagnosis submit, diagnosis actions

--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SymptomsForm from '../symptom-form/sympton-form';
+import Diagnosis from '../diagnosis/diagnosis';
 
 
 class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
-    console.log('Dashboard');
     return (
-            <section>
-                <Link to='/'><h2>DocTalk</h2></Link>
-                <p>Hello! You are logged in</p>
-                <SymptomsForm/>
-            </section>
+      <section>
+        <Link to='/'><h2>DocTalk</h2></Link>
+        <p>Hello! You are logged in</p>
+        <SymptomsForm/>
+        <Diagnosis/>
+      </section>
     );
   }
 }
--- src/component/symptom-form/sympton-form.js
+++ src/component/symptom-form/sympton-form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Symptoms from '../symptoms/symptoms';
-import searchActions from '../../action/search';
+import * as searchActions from '../../action/search';
 
 class SymptomForm extends React.Component {
   constructor(props) {
@@ -15,10 +15,7 @@ class SymptomForm extends React.Component {
     this.setState({[name]: value});
   };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const age = document.querySelector('input[name=age]').value;
-    const gender = document.querySelector('input[name=gender]').value;
+  constructSymptomString = (event) => {
     const inputFields = document.getElementsByTagName('input');
     const inputLength = inputFields.length;
     for (let i = 0; i < inputLength; i++) {
@@ -29,15 +26,41 @@ class SymptomForm extends React.Component {
     }
     const symptomString = `[${this.state.symptomKeys.toString()}]`;
 
-    const query = `https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=${symptomString}&gender=${gender}&year_of_birth=${age}&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imphc29uaGlAY3JhenlyZWRoZWFkLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiNDIyMiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAxOC0xMS0xMSIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNTQyMDc3OTc1LCJuYmYiOjE1NDIwNzA3NzV9.ApOeMgUijuHMxKnMoILKi4M9JBzSSI_ku4CVGsNX_kw&format=json&language=en-gb`;
-    console.log(query);
-    // return this.props.pTriggerDiagnosis(query)
-    //   .then(() => {
-    //
-    //   })
+    return symptomString;
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const symptoms = this.constructSymptomString(event);
+    const age = document.querySelector('input[name=age]').value;
+    const gender = document.querySelector('input[name=gender]').value;
+    let mediToken = null;
+    const currentDate = new Date();
+    const currentTime = currentDate;
+
+    if (this.props.mediToken !== null) {
+      if (currentTime - this.props.mediToken.timeStamp === 0) {
+        mediToken = this.props.mediToken.token;
+      } else {
+        pGetMediToken();
+        mediToken = this.props.mediToken.token;
+      }
+    } else {
+      pGetMediToken();
+      mediToken = this.props.mediToken.token;
+    }
+
+    const query = `https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=${symptoms}&gender=${gender}&year_of_birth=${age}&token=${mediToken}&language=en-gb`;
+
+    return this.props.pTriggerDiagnosis(query)
+      .then(() => {
+        this.props.resultsReady = true;
+      })
+      .catch(console.error(error));
   };
 
   render() {
+
     return (
       <form onSubmit={this.handleSubmit}>
         <h2>Get a Diagnosis</h2>
@@ -60,7 +83,8 @@ class SymptomForm extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  pTriggerDiagnosis: query => dispatch(searchActions.searchSymptoms(query)),
+  pGetDiagnosis: query => dispatch(searchActions.searchSymptoms(query)),
+  pGetMediToken: () => dispatch(searchActions.searchSymptoms()),
 });
 
 export default SymptomForm;
